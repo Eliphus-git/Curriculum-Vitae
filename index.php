a0e0b0dd0aeec650da04e97f56493281dbd07330
Eliphus-git <107413815+Eliphus-git@users.noreply.github.com>
2022-06-13 15:28:10 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>🙏😂Eliphus's Personal website😂🙏</title>
+  </head>
+  <body>
+  <center>  <table cellspacing = "30">
+      <tr>
+        <td><img src="Eliphus.png" alt="Eliphus Maruping" width="200" height="200" class="myImage"></td>
+        <td><h1 class="heading">Eliphus Maruping</h1>
+      <p><em>Engineer In Training @ <strong><a href="https://www.transnet.net/Divisions/Pages/FreightRail.aspx" target="_blank">Transnet Freight Rails</a></strong>.</em></p>
+      <p>An independent and self-motivated graduate trying to accomplish the engineering titles.</p></td>
+      </tr>
+    </table></center>
+    <hr size = "2" noshade>
+      <h3>Education:</h3>
+      <ol>
+       <li>Kakatlela Primary School</li>
+       <li>Nkang-Mahlale Secondary School</li>
+       <li>North-West University</li>
+      </ol>
+      <hr size = "2" noshade>
+      <h3>Skills:</h3>
+      <table>
+        <tr>
+          <td>C+++</td>
+          <td>🌝🌝🌚🌚🌚</td>
+        </tr>
+        <tr>
+          <td>Python</td>
+          <td>🌝🌝🌝🌚🌚</td>
+        </tr>
+        <tr>
+          <td>C-coding</td>
+          <td>🌝🌝🌝🌝🌚</td>
+        </tr>
+        <tr>
+          <td>Eletronics & Microcontrollers</td>
+          <td>🌝🌝🌝🌝🌝</td>
+        </tr>
+      </table>
+    <hr size = "2" noshade>
+    <h3>Curriculum Vitae:</h3>
+    <?php
+        require "CV.html"
+    ?>
+    <hr size = "2" noshade>
+  </body>
+</html>
